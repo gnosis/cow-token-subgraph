8a11729258e83337d5b8566563b62ee9d0055afd
Ben Smith <bh2smith@gmail.com>
2022-03-16 18:17:00 +0100

    include logic for holders and total supply

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,3 +1,114 @@
-import { Transfer } from "../generated/CowProtocolToken/CowProtocolToken";
+import { Address, BigInt, log } from "@graphprotocol/graph-ts";
+import {
+  Approval,
+  Transfer,
+} from "../generated/CowProtocolToken/CowProtocolToken";
+import { Holder, Supply } from "../generated/schema";
 
-export function handleTransfer(event: Transfer): void {}
+const COW_TOKEN = "0xDEf1CA1fb7FBcDC777520aa7f396b4E015F497aB";
+const NON_CIRCULATING = [
+  // Vested tokens (vCOW token address)
+  Address.fromString("0xD057B63f5E69CF1B929b356b579Cba08D7688048"),
+  // Solver Rewards
+  Address.fromString("0xA03be496e67Ec29bC62F01a428683D7F9c204930"),
+  // CoW DAO Safe
+  Address.fromString("0xcA771eda0c70aA7d053aB1B25004559B918FE662"),
+];
+
+export function loadOrCreateHolder(address: Address): Holder {
+  let holder = Holder.load(address.toHex());
+  if (!holder) {
+    holder = new Holder(address.toHex());
+    holder.account = address;
+  }
+  if (address != Address.zero()) {
+    // Skip creation of burn address holder.
+    holder.save();
+  }
+  return holder;
+}
+
+export function saveNonZero(holder: Holder): void {
+  if (holder.account != Address.zero()) {
+    holder.save();
+  } else {
+    log.info("Excluded Null address from internal!", []);
+  }
+}
+
+export function loadOrCreateSupply(): Supply {
+  let supply = Supply.load(COW_TOKEN);
+  if (!supply) {
+    // This will only ever happen once!
+    supply = new Supply(COW_TOKEN);
+  }
+  supply.save();
+  return supply;
+}
+
+export function nonCirculatingSupply(): BigInt {
+  const vestingContract = loadOrCreateHolder(
+    Address.fromString("0xD057B63f5E69CF1B929b356b579Cba08D7688048")
+  );
+  const devCoSafe = loadOrCreateHolder(
+    Address.fromString("0xA03be496e67Ec29bC62F01a428683D7F9c204930")
+  );
+
+  return vestingContract.balance.plus(devCoSafe.balance);
+}
+
+export function supplyTriggeringTransfer(from: Address, to: Address): bool {
+  const zero = Address.zero();
+  const mintOrBurn = from == zero || to == zero;
+  const involvesIgnoredAccounts =
+    NON_CIRCULATING.includes(from) || NON_CIRCULATING.includes(to);
+
+  return mintOrBurn || involvesIgnoredAccounts;
+}
+
+export function updateSupply(from: Address, to: Address, amount: BigInt): void {
+  log.warning("Got supply update event {}, {}, {}", [
+    from.toHex(),
+    to.toHex(),
+    amount.toString(),
+  ]);
+  const supply = loadOrCreateSupply();
+  if (from == Address.zero()) {
+    log.info("Token Minted {}", [amount.toString()]);
+    supply.total = supply.total.plus(amount);
+    supply.circulating = supply.circulating.plus(amount);
+  }
+  if (to == Address.zero()) {
+    log.info("Token Burned {}", [amount.toString()]);
+    supply.total = supply.total.minus(amount);
+    supply.circulating = supply.circulating.minus(amount);
+  }
+
+  if (NON_CIRCULATING.includes(from)) {
+    log.info("Circulating Supply Increase {}", [amount.toString()]);
+    supply.circulating = supply.circulating.plus(amount);
+  }
+
+  if (NON_CIRCULATING.includes(to)) {
+    log.info("Circulating Supply Decrease {}", [amount.toString()]);
+    supply.circulating = supply.circulating.minus(amount);
+  }
+  supply.save();
+}
+
+export function handleTransfer(event: Transfer): void {
+  const from = event.params.from;
+  const to = event.params.to;
+  const sender = loadOrCreateHolder(from);
+  const receiver = loadOrCreateHolder(to);
+  const amount = event.params.value;
+
+  if (supplyTriggeringTransfer(from, to)) {
+    updateSupply(from, to, amount);
+  }
+
+  sender.balance = sender.balance.minus(amount);
+  receiver.balance = receiver.balance.plus(amount);
+  saveNonZero(sender);
+  saveNonZero(receiver);
+}
